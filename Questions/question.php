4473a296c14cb9486ead68f9cb39597751e9fcce
GabrielaSaboia <your_email@example.com>
2019-10-23 12:24:10 -0400

    Further php scripting and some html fixes.

--- Questions/question.php
+++ Questions/question.php
@@ -2,6 +2,29 @@
 $name = filter_input(INPUT_POST, 'questionName');
 $body = filter_input(INPUT_POST, 'body');
 $skills= filter_input(INPUT_POST, 'skills');
+
+$skillCheck = strpos($skills, ',');
+
+if($_SERVER["REQUEST_METHOD"] == 'POST'){
+    //Checking name input
+    if(empty($name)){
+        $nameError = 'Please enter a name.';
+    }else if(strlen($name) < 3){
+        $nameErrorTwo= 'Name must be at least 3 characters long.';
+    }
+    //Checking Body content
+    if(empty($body)){
+        $bodyError = 'Please enter text.';
+    }else if(strlen($body)>500){
+        $bodyError ='Text must be shorter than 500 characters.';
+    }
+    //Checking skills section
+    if(empty($skills)){
+    $skillError = 'Please add a skill.';
+    }else if($skillCheck === false){
+     $skillError = 'Please enter more skills.';
+    }
+}
 ?>
 
 <html>
