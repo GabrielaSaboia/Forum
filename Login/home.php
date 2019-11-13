9bc00243e5c60c8ace6406726504e2735076cf53
GabrielaSaboia <your_email@example.com>
2019-11-13 07:00:06 -0500

    added a navigation bar and background color

--- Login/home.php
+++ Login/home.php
@@ -14,6 +14,7 @@
     <script src="https://use.fontawesome.com/releases/v5.0.7/js/all.js"></script>
 
     <link rel="stylesheet" type="text/css" href="../css/main.css">
+    <link rel="stylesheet" type="text/css" href="../css/nav.css">
 </head>
 <body>
 <div class="nav-bar">
@@ -21,7 +22,7 @@
         <ul>
             <li><a href="home.php">Home</a></li>
             <li><a href="../Questions/profile.php">Profile</a></li>
-            <li><a href="../Registration/account.php">Account</ahref></li>
+            <li><a href="../Registration/account.php">Account</a></li>
         </ul>
     </nav>
 </div>
@@ -32,7 +33,7 @@
 
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
         integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
-        crossorigin="anonymous"></link>
+        crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
             integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1"
             crossorigin="anonymous"></script>
