0780b47c1b59634d8c949c1f137977b0e4175eec
GabrielaSaboia <your_email@example.com>
2020-01-10 19:37:57 -0500

    final adjustments

--- views/nav.php
+++ views/nav.php
@@ -2,18 +2,16 @@
 ?>
 <div>
     <nav class="navbar navbar-expand-md">
-        <ul>
-            <div class="where left">
-                <div class="nav-logo logo-img">
-                    <img src="img/logo.png">
-                </div>
-                <li><a href="index.php?action=display_questions"">Home</a></li>
-                <li><a href=".?action=show_question_form">Ask Something</a></li>
+        <ul class="list-inline">
+            <div class="placement">
+                <li>
+                    <div class="nav-logo logo-img"><img src="img/logo.png"></div>
+                </li>
                 <li>
                     <form action="index.php" method="post">
                         <input type="hidden" name="action" value="logout">
                         <input type="hidden" name="userId" value="<?php echo $userId; ?>">
-                        <input type="submit" value="Logout">
+                        <input class="btn" type="submit" value="Logout">
                     </form>
                 </li>
             </div>
